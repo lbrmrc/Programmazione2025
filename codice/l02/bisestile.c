44bbecf6be7995b55de847eb7be45e3c07a75a5b
Marco Alberti <marco.alberti@unife.it>
2025-10-09 14:37:31 +0200

    bisestile

---
+++ codice/l02/bisestile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int anno;
+  printf("Inserisci l'anno\n");
+  scanf("%d", &anno);
+  if (anno % 4 == 0 && anno % 100 != 0 || anno % 400 == 0)
+    printf("Bisestile\n");
+  else
+    printf("Non bisestile\n");
+}
