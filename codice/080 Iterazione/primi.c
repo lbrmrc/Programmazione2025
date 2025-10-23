02a2569eb7ad50b45a50fc593b0042c2bf50f646
Marco Alberti <marco.alberti@unife.it>
2025-10-23 16:18:05 +0200

    ottimizzazione

--- codice/080 Iterazione/primi.c
+++ codice/080 Iterazione/primi.c
@@ -5,8 +5,10 @@ int primo(int n) {
   int i;                  // elemento della sequenza [1, 2, ..., n]
   int conteggio_divisori; // accumulatore
   conteggio_divisori = 0; // inizializzazione accumulatore
-  for (i = 1; i <= n; i++) {
+  for (i = 2; i * i <= n; i++) {
     // i prende i valori [1, 2, ..., n]
+    if (conteggio_divisori > 0)
+      return 0;
     if (n % i == 0) {
       // i prende i valori di filter(divide n, [1, ..., n])
       // cioè dei divisori di n
@@ -15,7 +17,7 @@ int primo(int n) {
   }
   // conteggio_divisori = reduce(++, 0, filter(divide n, [1, ..., n])) cioè il
   // numero di divisori di n;
-  if (conteggio_divisori == 2)
+  if (conteggio_divisori == 0)
     return 1;
   else
     return 0;
