966ee27ab2feeac64af520a0b43e9dd0d30b1425
Dominik Miotla <dominik.miotla@edu.unife.it>
2025-12-04 09:33:44 +0100

    Add tutorato trasversale

---
+++ tutorato/tutorato_trasversale_04_12_25/Errori_Compilazione/read_input_E1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define DIM 10
+
+int main(){
+    int a[DIM];
+
+    printf("Insersci gli elementi dell'array:\n");
+    for(i = 0; i<DIM; i++){
+        printf("\t-Inserisci l'elemento %d-esimo: ",i+1);
+        scanf("%d",&a[i]);
+    }
+    
+    printf("\nGli elementi dell'array sono: ");
+    for(i=0; i<DIM; i++)
+        printf("%d ",*(a+i));
+    printf("\n");
+    return 0;
+}
