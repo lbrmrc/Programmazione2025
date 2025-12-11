807fb764721361791a79f5732317389a72eff976
Marco Alberti <marco.alberti@unife.it>
2025-12-11 15:49:03 +0100

    soluzione parte 1

--- esami/20250116/main.c
+++ esami/20250116/main.c
@@ -1,3 +1,36 @@
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "listaClienti.h"
+
+int main(int argc, char *argv[]){
+    FILE *pf;
+    Record r;
+    Lista listaclienti;
+
+    if (argc != 2){
+        printf("Uso: %s fileRilevamenti\n", argv[0]);
+        exit(1);
+    }
+
+    // apertura
+
+    pf = fopen(argv[1], "rb");
+    if (pf == NULL){
+        printf("Errore apertura %s\n", argv[1]);
+        exit(2);
+    }
+
+    nuovaLista(&listaclienti);  // inizializzazione accumulatore
+    // accesso al file
+    while (fread(&r, sizeof(Record), 1, pf) == 1){
+        // dentro r c'è un record del file
+        aggiorna(&listaclienti, r);
+    }
+
+    // chiusura
+    fclose(pf);
+
+    stampa(listaclienti);
     return 0;
 }
